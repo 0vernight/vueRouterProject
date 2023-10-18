dbdfad8c49723a116e1c888eaa805cd3e786d6fe
0vernight <2323659522@qq.com>
2023-10-18 10:57:26 +0800

    Changes to be committed: 	new file:   .hintrc 	modified:   src/App.vue 	new file:   src/components/MyIndex.vue 	modified:   src/router/index.ts 	new file:   src/views/MyIndex.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+// import MyIndex from './components/MyIndex.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <!-- <RouterLink to="/MyIndex">myindex</RouterLink> -->
       </nav>
     </div>
   </header>
---
+++ src/views/MyIndex.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import MyIndex from '../components/MyIndex.vue'
+</script>
+
+<template>
+  <main>
+    <MyIndex></MyIndex>
+  </main>
+</template>
